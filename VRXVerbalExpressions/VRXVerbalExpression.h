a2f184a6062d938f48b0b4669244f253328941f5
Jackson Harper <jacksonh@gmail.com>
2014-07-02 14:32:41 -0400

    headers

--- VRXVerbalExpressions/VRXVerbalExpression.h
+++ VRXVerbalExpressions/VRXVerbalExpression.h
@@ -3,7 +3,7 @@
 //  VRXVerbalExpressions
 //
 //  Created by Jackson Harper on 8/9/13.
-//  Copyright (c) 2013 Syntax Tree. All rights reserved.
+//  Copyright (c) 2013 Harper Semiconductors, Inc.
 //
 
 #import <Foundation/Foundation.h>
